0e41f10b48f9c6ac620d1b28f1ad63770d72330f
Augustin Gerasym <augustingerasym@gmail.com>
2025-01-16 22:06:39 +0100

    Initial commit

---
+++ src/test/java/com/remindmelater/usermanagement/UserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remindmelater.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
